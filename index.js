8f97af46abc8d840b5c316a28e14f9e3bd326e92
DESKTOP-T9NLHTA\Sudeera <sudeera.karunathilaka@gmail.com>
2018-05-23 22:20:35 +1000

    Lecture 151 - Exception handeling and loging

--- index.js
+++ index.js
@@ -2,12 +2,62 @@
 const express = require('express');
 const app = express();
 
+//Winston to log
+const winston = require('winston');
+require('winston-mongodb'); //to log errors in to a mongo db
+
+
+//Use "express-async-errors" to module to handle all errors. No separate error handlers needed.
+require('express-async-errors');
+
+const error = require('./middleware/error');
+
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
 const login = require('./routes/login');
+
+/*
+//This is to handle any uncaught exceptions.
+process.on('uncaughtException', (ex) => {
+    winston.error(ex.message, ex);
+    process.exit(1);
+});
+
+//This is to handle any uncaught exceptions.
+process.on('unhandledRejection', (ex) => {
+    winston.error(ex.message, ex);
+    process.exit(1);
+});
+Both can be replaced by winston as follows
+*/
+
+winston.handleExceptions(new winston.transports.File({ filename: 'unhandled-exceptions.log'}));
+process.on('unhandledRejection', (ex) => {
+    throw ex;//winston.handleExceptions() does not handle "unhandledRejection" errors. 
+    //When this exception is thrown, it will handled by winston.handleExceptions()
+});
+
+
+
+//This is to handle "express" related exceptions
+winston.add(winston.transports.File, {filename: 'logfile.log'});
+winston.add(winston.transports.MongoDB, {
+    db: 'mongodb://localhost/vidly_log',
+    level: 'error' 
+    /*
+    log levels
+        error
+        warn
+        info
+        verbose
+        debug
+        silly
+    If we use "info" it will log "error", "warn" and "info"
+    */
+})
 
 //Read the config files to get jwt-private-key
 if(!process.env.jwtPrivateKey){
@@ -47,6 +97,8 @@ app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 //Use auth api's 
 app.use('/api/login', login);
+
+app.use(error);
 
 
 const port = process.env.PORT || 3000;
