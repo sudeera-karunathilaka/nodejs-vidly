aabf8d60017a9e2d61b4ef249bf8ee35e992df36
Sudeera Poramba Liyanage <sudeera.porambaliyanage@team.telstra.com>
2018-05-29 13:12:27 +1000

    Refactored the test.

--- test/integration/genre.test.js
+++ test/integration/genre.test.js
@@ -3,10 +3,14 @@ const {Genre} = require('../../models/genre');
 const {User} = require('../../models/user');
 
 let server;
+let token;
+let genre;
 describe('/api/genre', () => {
     // This will make sure the server is closed after each test suit execution and reload before the next test suit execution.
     // If not it will try to open the already opened port and result in "Port already in user" error
-    beforeEach(() => {server = require('../../index'); });
+    beforeEach(() => {
+        server = require('../../index');
+    });
     afterEach( async() => {
         server.close();
         await Genre.remove({});//Remove all dummy data at the end on each test suit execution.
@@ -47,33 +51,49 @@ describe('/api/genre', () => {
         });
     });
 
+    const exec = async () => {
+        return await request(server)
+            .post('/api/genres')
+            .set('x-auth-token', token)
+            .send(genre);
+    }
+
     describe('POST /', () =>{
+
+        beforeEach(async () => {
+            token = new User().generateAuthToken();
+            genre = {name: 'Genre 1'};
+        })
+
+
         it('should return 401 if client is not logged in', async () => {
-            const genre = {name: 'Genre 1'};
-            const response = await request(server).post('/api/genres').send(genre);
+            token = '';
+            const response = await exec();
             expect(response.status).toBe(401);
         });
 
         it('should return 400 if genre name is less than 5 characters', async () => {
-            const token = new User().generateAuthToken();
-            console.log('Token.............. ' + token);
-            const genre = {name: '123'};
-            const response = await request(server)
-                            .post('/api/genres')
-                            .set('x-auth-token', token)
-                            .send(genre);
+            genre = {name: '123'};
+            const response = await exec();
             expect(response.status).toBe(400);
         });
 
         it('should return 400 if genre name is more than 50 characters', async () => {
-            const token = new User().generateAuthToken();
-            console.log('Token.............. ' + token);
-            const genre = {name: new Array(52).join('a')}; //Create a string of 51 characters
-            const response = await request(server)
-                            .post('/api/genres')
-                            .set('x-auth-token', token)
-                            .send(genre);
+            genre = { name: new Array(52).join('a') }; //Create a string of 51 characters
+            const response = await exec();
             expect(response.status).toBe(400);
+        });
+
+        it('should should save the genre if it is valid', async () => {
+            const response = await exec();
+            genre = await Genre.find({name: 'Genre 1'});
+            expect(genre).not.toBeNull();
+        });
+
+        it('should return the  genre if it is valid', async () => {
+            const response = await exec();
+            expect(response.body).toHaveProperty('_id');
+            expect(response.body).toHaveProperty('name', 'Genre 1');
         });
     });
 });
